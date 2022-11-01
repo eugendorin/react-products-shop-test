a000dfa69b65c7734c20b3b1a97cab90ca05e5d6
Eugen Dorin <dorineugen93@gmail.com>
2022-11-01 08:50:06 +0200

    Save shopping cart to browser storage

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,9 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { loadState, saveState } from "utils";
+
+const BROWSER_STORAGE_CART_KEY = "shopping_cart";
+const saveBrowserCartStorage = (state) => saveState(BROWSER_STORAGE_CART_KEY, state);
+const getBrowserCartStorage = () => loadState(BROWSER_STORAGE_CART_KEY);
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cart: []
+    cart: getBrowserCartStorage() ?? []
   },
   reducers: {
     addProduct: (state, action) => {
@@ -13,16 +18,19 @@ export const cartSlice = createSlice({
       } else {
         state.cart.push({ ...action.payload, quantity: 1 });
       }
+      saveBrowserCartStorage(state.cart);
     },
     removeProduct: (state, action) => {
       const itemsRemainingInCart = state.cart.filter((item) => item.id === action.payload.id);
       state.cart = itemsRemainingInCart;
+      saveBrowserCartStorage(state.cart);
     },
     updateQuantity: (state, action) => {
       const updatedItemsInCart = state.cart.map((item) =>
         item.id === action.payload.id ? (item.quantity = action.payload.updatedQuantity) : item
       );
       state.cart = updatedItemsInCart;
+      saveBrowserCartStorage(state.cart);
     }
     // applyVoucher: (state, action) => {
     //   //
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
-export * from "./formatPrice";
+export * from "./format-price";
+export * from "./browser-storage";
